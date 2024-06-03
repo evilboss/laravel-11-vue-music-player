0585bd1cece4044e91f8a916eded5691e7592785
evilboss <jr.evilboss@gmail.com>
2024-06-04 00:24:47 +0800

    initialize project

---
+++ README.md
@@ -0,0 +1,33 @@
+![alt text](./public//tech.png)
+
+### Setup Locally
+
+```
+cp .env.example .env
+```
+
+```
+composer install
+```
+
+```
+php artisan migrate
+```
+
+```
+php artisan key:generate
+```
+
+```
+php artisan serve
+```
+
+### Client :
+
+```
+npm install
+```
+
+```
+npm run dev
+```
---
+++ resources/js/router/index.js
@@ -0,0 +1,21 @@
+import {createRouter, createWebHistory} from "vue-router";
+
+import login from '../components/LoginPage.vue';
+import register from '../components/RegisterPage.vue';
+import songList from '../components/SongList.vue';
+
+console.log('router loaded')
+
+const routes = [
+    {path: '/login', component: login},
+    {path: '/register', component: register},
+    {path: '/songs', component: songList},
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    linkActiveClass: 'active',
+    routes,
+});
+
+export default router;
