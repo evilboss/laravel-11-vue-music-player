56e5c5ee1d7f83419aa3c1e8a497c85c25124edd
evilboss <jr.evilboss@gmail.com>
2024-06-04 00:58:16 +0800

    update song table seeder and add main router

--- database/migrations/2024_06_03_162626_create_songs_table.php
+++ database/migrations/2024_06_03_162626_create_songs_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('duration'); // Assuming duration is required
             $table->integer('durationInSeconds'); // Assuming durationInSeconds is required
             $table->timestamps();
-
         });
     }
 
